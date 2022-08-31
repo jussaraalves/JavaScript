f393bb93cd1e0048d369c7b97f6d1637e08f5405
jussaraalves <jussaraalves852@gmail.com>
2022-08-31 20:59:50 -0300

    Condições (Parte 1)

---
+++ exercicios/ex008/script.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 60){ //condiçao simles!
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO')
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
